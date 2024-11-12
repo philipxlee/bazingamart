40fd2988058320e6762396a2380d69681e1e9ce7
lucy kopin <lbk15@duke.edu>
2024-11-11 20:21:06 -0500

    removed duplicates

--- db/load.sql
+++ db/load.sql
@@ -1,4 +1,4 @@
-\COPY Users(id, email, password, firstname, lastname) FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
 -- since id is auto-generated; we need the next command to adjust the counter
 -- for auto-generation so next INSERT will not clash with ids loaded above:
 SELECT pg_catalog.setval('public.users_id_seq',
@@ -14,25 +14,14 @@ SELECT pg_catalog.setval('public.products_product_id_seq',
 SELECT pg_catalog.setval('public.reviews_review_id_seq',
                          (SELECT MAX(review_id)+1 FROM reviews),
                          false);
-\COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.reviews_review_id_seq',
-                         (SELECT MAX(review_id)+1 FROM reviews),
-                         false);
 
-\COPY Cart(order_id, user_id, created_at, total_price, purchase_status, coupon_code) FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV;
-SELECT pg_catalog.setval('public.cart_order_id_seq',
-                         (SELECT MAX(order_id) + 1 FROM Cart),
-                         false);
-\COPY Cart(order_id, user_id, created_at, total_price, purchase_status, coupon_code) FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV;
+\COPY Cart FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV;
 SELECT pg_catalog.setval('public.cart_order_id_seq',
                          (SELECT MAX(order_id) + 1 FROM Cart),
                          false);
 
-\COPY CartProducts(order_id, product_id, quantity, unit_price) FROM 'CartProducts.csv' WITH DELIMITER ',' NULL '' CSV;
+\COPY CartProducts FROM 'CartProducts.csv' WITH DELIMITER ',' NULL '' CSV;
 
-\COPY Coupons(coupon_code, discount_percentage) FROM 'Coupons.csv' WITH DELIMITER ',' CSV;
-\COPY CartProducts(order_id, product_id, quantity, unit_price) FROM 'CartProducts.csv' WITH DELIMITER ',' NULL '' CSV;
-
-\COPY Coupons(coupon_code, discount_percentage) FROM 'Coupons.csv' WITH DELIMITER ',' CSV;
+\COPY Coupons FROM 'Coupons.csv' WITH DELIMITER ',' CSV;
 
 --\COPY Inventory FROM 'Inventory.csv' WITH DELIMITER ',' NULL '' CSV;
