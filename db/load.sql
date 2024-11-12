7f9f28cdd7f821fd009a1101f8a45c5b6f0319fa
lucy kopin <lbk15@duke.edu>
2024-11-11 21:43:58 -0500

    add generated data

--- db/load.sql
+++ db/load.sql
@@ -1,21 +1,20 @@
-\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
--- since id is auto-generated; we need the next command to adjust the counter
--- for auto-generation so next INSERT will not clash with ids loaded above:
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV;
+-- Adjusting the sequence for Users' auto-generated primary key:
 SELECT pg_catalog.setval('public.users_id_seq',
-                         (SELECT MAX(id)+1 FROM Users),
+                         (SELECT MAX(id) + 1 FROM Users),
                          false);
 
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV;
+-- Adjusting the sequence for Products' auto-generated primary key:
 SELECT pg_catalog.setval('public.products_product_id_seq',
-                         (SELECT MAX(product_id)+1 FROM Products),
+                         (SELECT MAX(product_id) + 1 FROM Products),
                          false);
 
-\COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.reviews_review_id_seq',
-                         (SELECT MAX(review_id)+1 FROM reviews),
-                         false);
+\COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV;
+-- Adjusting the sequence for Reviews' auto-generated primary key:
 
 \COPY Cart FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV;
+-- Adjusting the sequence for Cart's auto-generated primary key:
 SELECT pg_catalog.setval('public.cart_order_id_seq',
                          (SELECT MAX(order_id) + 1 FROM Cart),
                          false);
@@ -24,4 +23,5 @@ SELECT pg_catalog.setval('public.cart_order_id_seq',
 
 \COPY Coupons FROM 'Coupons.csv' WITH DELIMITER ',' CSV;
 
---\COPY Inventory FROM 'Inventory.csv' WITH DELIMITER ',' NULL '' CSV;
+\COPY Orders FROM 'Orders.csv' WITH DELIMITER ',' NULL '' CSV;
+
