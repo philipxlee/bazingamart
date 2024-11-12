b0aee9c0b3856f57f666c4986d40af1559f8991e
Philip Lee <kl445@duke.edu>
2024-11-12 12:00:55 -0500

    Added pagination for products

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,5 @@
 from flask import current_app as app
+from math import ceil
 
 class Product:
     def __init__(self, product_id, product_name, price, available):
@@ -18,15 +19,30 @@ class Product:
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
-    def get_all(available=True):
-        rows = app.db.execute('''
+    def get_all(available=True, page=1, per_page=9):
+        offset = (page - 1) * per_page
+        rows = app.db.execute(
+            '''
             SELECT product_id, product_name, price, available
             FROM Products
             WHERE available = :available
-        ''',
-        available=available)
-        return [Product(*row) for row in rows]
-    
+            LIMIT :per_page OFFSET :offset
+            ''',
+            available=available, 
+            per_page=per_page, 
+            offset=offset
+        )
+        
+        total_count = app.db.execute(
+            '''
+            SELECT COUNT(*) FROM Products WHERE available = :available
+            ''', 
+            available=available
+        )[0][0]
+        
+        total_pages = ceil(total_count / per_page)
+        return [Product(*row) for row in rows], total_pages
+
     @staticmethod
     def get_top_k_expensive(k):
         rows = app.db.execute('''
